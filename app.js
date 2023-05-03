85b04f898857fb09311022fd6be50032431794ef
Juljeta Troka <juliet.troka@gmail.com>
2023-05-03 19:03:49 +0100

    added functionality by randomly select box and show icon

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ const players = document.querySelector(".players");
 window.onload = () => {
 
     for (let i = 0; i < allBox.length; i++) {
-        allBox[i].setAttribute("onclick","clickedBox(this)")
-        
+        allBox[i].setAttribute("onclick", "clickedBox(this)")
+
     }
     selectBtnX.onclick = () => {
         selectBox.classList.add("hide");
@@ -26,13 +26,36 @@ window.onload = () => {
 let playerXIcon = "fas fa-times";
 let playerOIcon = "far fa-circle";
 
-function clickedBox(element){
-    if(players.classList.contains("player")){
+function clickedBox(element) {
+    if (players.classList.contains("player")) {
         element.innerHTML = `<i class="${playerOIcon}"></i>`;
         players.classList.add("active");
-    }else {
+    } else {
         element.innerHTML = `<i class="${playerXIcon}"></i>`;
         players.classList.add("active");
     }
-    element.style.pointerEvents ="none";
+    element.style.pointerEvents = "none";
+    let randomDelayTime = ((Math.random() * 1000) + 200).toFixed()
+    setTimeout(() => {
+        bot();
+    }, randomDelayTime);
+}
+
+function bot() {
+    let array = [];
+    for (let i = 0; i < allBox.length; i++) {
+        if (allBox[i].childElementCount == 0) {
+            array.push(i);
+        }
+    }
+    let randomBox = array[Math.floor(Math.random() * array.length)]
+    if (array.length > 0) {
+        if (players.classList.contains("player")) {
+            allBox[randomBox].innerHTML = `<i class="${playerXIcon}"></i>`;
+            players.classList.add("active");
+        } else {
+            allBox[randomBox].innerHTML = `<i class="${playerOIcon}"></i>`;
+            players.classList.add("active");
+        }
+    }
 }
